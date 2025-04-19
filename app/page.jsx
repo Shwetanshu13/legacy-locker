5c99dbb56e48d70e84147a5913520176db861696
Anil Kumar Meena <146952978+J33rry@users.noreply.github.com>
2025-04-19 13:57:02 +0530

    added the encrypt/decrypt function

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,24 @@
 import Landing from "@/components/Landing";
 import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
+// import ImageForm from "@/components/add-new-legacy/ImageForm";
 // import ImageForm from "@/components/add-new-legacy/ImageForm";
 
 export default function Home() {
     return (
         <main>
             <Landing />
+        </main>
+    );
+    return (
+        <main>
+            <Navbar />
+
+            <div className="flex min-h-screen flex-col items-center justify-between p-24">
+                <h1 className="text-3xl font-bold underline">Hello world!</h1>
+                {/* <ImageForm /> */}
+            </div>
         </main>
     );
 }
