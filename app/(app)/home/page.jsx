046877d98afb286091bcf35b1b65c7d7196339b3
Shwetanshu13 <shwetanshusinha13@gmail.com>
2025-04-20 12:44:43 +0530

    Added time trigger

--- app/(app)/home/page.jsx
+++ app/(app)/home/page.jsx
@@ -1,6 +1,16 @@
 import Dashboard from "@/components/Dashboard";
+import { useUser } from "@clerk/nextjs";
 
 function Home() {
+  const { user, isLoaded } = useUser();
+  useEffect(() => {
+    if (isLoaded) {
+      axios.post("/api/vault/update-activity", {
+        clerkUserId: user.id,
+      });
+    }
+  }, [isLoaded]);
+
   return (
     <div>
       <Dashboard />
