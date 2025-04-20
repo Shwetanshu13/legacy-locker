0cee8c8f33f83e4487d857674f77e24c6425d64b
Anil Kumar Meena <146952978+J33rry@users.noreply.github.com>
2025-04-20 09:11:34 +0530

    styled the Navbar

--- app/(app)/add-new-legacy/password/page.jsx
+++ app/(app)/add-new-legacy/password/page.jsx
@@ -1,12 +1,12 @@
 import Password from "@/components/add-new-legacy/Password";
 import React from "react";
 
-const Vault = () => {
-  return (
-    <div>
-      <Password />
-    </div>
-  );
-};
-
-export default Vault;
+export default function Vault() {
+    {
+        return (
+            <div>
+                <Password />
+            </div>
+        );
+    }
+}
