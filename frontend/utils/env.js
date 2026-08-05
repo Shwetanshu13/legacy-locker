6aabbd18c963f25df49b2a1532b49f889e0aaf3f
Shwetanshu13 <shwetanshusinha13@gmail.com>
2026-08-05 15:16:49 +0530

    next.js env var loading issue fixed

--- frontend/utils/env.js
+++ frontend/utils/env.js
@@ -1,15 +1,10 @@
-const requiredEnvVars = [
-    'NEXT_PUBLIC_API_URL',
-    'SECRET_KEY'
-];
+const publicEnv = {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_SOMETHING_ELSE: process.env.NEXT_PUBLIC_SOMETHING_ELSE,
+};
 
-for (const envVar of requiredEnvVars) {
-    if (!process.env[envVar]) {
-        throw new Error(`FATAL: Missing required frontend environment variable: ${envVar}`);
-    }
+for (const [key, value] of Object.entries(publicEnv)) {
+    if (!value) throw new Error(`FATAL: Missing required frontend environment variable: ${key}`);
 }
 
-export const env = {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    SECRET_KEY: process.env.SECRET_KEY,
-};
+export { publicEnv as env };
